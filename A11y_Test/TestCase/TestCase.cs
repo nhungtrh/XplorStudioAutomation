b41b3a427ddab0102f9518f03720c208bc5fd226
nhungtrh <tranhongnhung0408@gmail.com>
2023-03-23 17:20:47 +0700

    schedule

--- A11y_Test/TestCase/TestCase.cs
+++ A11y_Test/TestCase/TestCase.cs
@@ -27,6 +27,8 @@ namespace XplorStudio.TestCase
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://dev.pre.studio.xplor.co/login/");
 
+            //login
+
             IWebElement element = driver.FindElement(By.Id("inputEmail"));
             element.SendKeys("mpv@test.com");
 
@@ -36,8 +38,33 @@ namespace XplorStudio.TestCase
             IWebElement element2 = driver.FindElement(By.XPath("//*[@type = 'submit' and @class = 'btn btn-primary btn-lg']"));
             element2.Click();
 
-            IWebElement elemen4 = driver.FindElement(By.XPath("//*[@type = 'submit' and @class = 'btn btn-primary btn-lg']"));
+            //get page title 
+            String title = driver.Title;
+
+
+            //add new event
+            driver.Navigate().GoToUrl("http://dev.pre.studio.xplor.co/app/schedule/");
+            driver.Url = "http://dev.pre.studio.xplor.co/app/schedule/";
+
+            IWebElement element3 = driver.FindElement(By.Id("addEventsButton"));
+            element3.Click();
+
+            IWebElement element4 = driver.FindElement(By.Id("add-class-icon"));
             element4.Click();
+
+            //add new remember
+
+            //IWebElement element4 = driver.FindElement(By.XPath("//*[@class='btn btn-primary create-first-reminder']"));
+            //element4.Click();
+
+            //IWebElement element5 = driver.FindElement(By.Id("addEventsButton"));
+            //element5.SendKeys("03/23/2023");
+
+            //IWebElement element6 = driver.FindElement(By.Id("dp1679501598747"));
+            //element6.SendKeys("03/23/2023");
+
+
+
         }
      
 
